d0ab72b76616cb6cbb38b671962c3449e4738975
JosePrositoxz <jose.granda@uerdf.edu.ec>
2025-11-17 08:27:31 -0500

    Calculadora Jose V1

---
+++ CalculadoraJoseV1.py
@@ -0,0 +1,14 @@
+print("Calculadora de operaciones básicas.")
+
+num1 = int(input("Ingresa un número: "))
+num2 = int(input("Ingresa otro número: "))
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print(f"\nSuma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicación: {multiplicacion}")
+print(f"División: {division}")
